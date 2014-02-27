0886f7314f75c08cfae71333248b6196a422796a
Squidbelt <Daniel.Taylor2@Comcast.net>
2014-02-27 11:51:18 -0700

    Adjusted the server a bit, implemented login
    Added getting an IP Address from the client, as well as implementation of the ArrayList for IPs.

--- src/crypt/chat/Server.java
+++ src/crypt/chat/Server.java
@@ -5,10 +5,30 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 
+private String username;
+private String password;
+private String ipAddress;
+private ArrayList<String> ipList = new ArrayList();
+
 public class Server {
 
-	serve = new ServerSocket(port);
-	System.out.println("Congratulations you have started a server on port: "+serve.getLocalPort()+"\nThe server can be found at the following IP adress: "+serve.getInetAddress());
+	public void Login(String user, String pass) {
+		this.username = user;
+		this.password = pass;
+		try {
+			InetAddress i = InetAddress.getLocalHost();
+			this.ipAddress = i.getHostAddress();
+			ipList.add(ipAddress);
+			System.out.println(username + " connected from " + ip);
+		
+		} catch (Exception e) {}
+	}
+
+
+}
+
+/*	serve = new ServerSocket(port);
+	System.out.println("Congratulations you have started a server on port: "+serve.getLocalPort()+"\n The server can be found at the following IP adress: "+serve.getInetAddress());
 	client=serve.accept();
 	
 	//makes sure that the client has showed up
@@ -16,17 +36,11 @@ public class Server {
 	String temp = reader.readLine();
 	if(temp.equals("Hello server"))
 	{
-		System.out.println("Congrats you have an oponet!");
-		PrintStream pushToSocket = new PrintStream(client.getOutputStream());
-		pushToSocket.println("Got something");
-		pushToSocket.println(numOfGames);
+		System.out.println()
 	}else
 	{
 		System.out.println("Oops! Something went wrong. Please restart the program. Sorry about that! :(");
 	}
-	player1.setSocket(client);
-	//sets who moves first
-	System.out.println("Do you want to move first? y/n");
 	temp=input();
 	PrintStream pushToSocket = new PrintStream(client.getOutputStream());
 	if(temp.equalsIgnoreCase("y"))
@@ -39,5 +53,5 @@ public class Server {
 		player1.setMovesFirst(true);
 		pushToSocket.println("false");
 	}
-	
+*/
 }
