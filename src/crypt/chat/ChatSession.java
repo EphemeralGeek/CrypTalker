8143e597a1ec867ef23806360cd4edeac189f9e4
CorruptingAcid <corruptingacid@gmail.com>
2014-02-28 12:48:41 -0700

    updating login and communications

--- src/crypt/chat/ChatSession.java
+++ src/crypt/chat/ChatSession.java
@@ -8,4 +8,29 @@ import java.net.Socket;
 public class ChatSession {
 	private Socket client;
 	private String serverName;
+	private String username;
+	private String password;
+	
+	ChatSession(Socket s,  String server, String user, String pass)
+	{
+		this.client = s;
+		this.serverName = server;
+		this.username=user;
+		this.password=pass;		
+	}
+	
+	protected boolean logIn(String user, String pass)
+	{
+		
+		//returns false if login fails
+		//rturn true if login suceeds
+	}
+	
+	protected boolean sendChat(String message)
+	{
+		//sends stuff to output stream
+		this.client.getInputStream();
+		
+		
+	}
 }
